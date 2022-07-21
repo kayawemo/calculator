fd935f165a7a64c123079342aed88a65fc6a4286
Kayode-Awe <kayode.awe@outlook.com>
2022-07-20 22:19:45 -0600

    develop index.html and style.css for the calculator

---
+++ script/calculator.js
@@ -0,0 +1,6 @@
+const numberButtons = document.querySelectorAll(".data-number");
+const operatorButtons = document.querySelectorAll(".data-operator");
+const equalButton = document.querySelectorAll(".data-equal");
+const deleteButton = document.querySelector(".data-delete");
+const clearButton = document.querySelector(".data-clear");
+const firstNumber = document
